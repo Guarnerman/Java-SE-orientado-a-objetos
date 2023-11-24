dcc35562876097fa23a5dbe2eac085d07f18f134
guarnerman <gs.ulises18@gmail.com>
2023-11-23 22:40:14 -0600

    Creación de un package virtual para una UIMenu. Creación de elementos estáticos y entendimiento del scope tanto para public como para static

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import static UI.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor("Sócrates");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Vicente Fernandez";
         myDoctor.showName();
+        myDoctor.showId();
+
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();;
+
+        showMenu();
     }
 }
